f5dc0a7c2e98148b64cc7e2e8e1df538e1fba534
ollylll97lll <58421531+ollylll97lll@users.noreply.github.com>
2022-07-15 10:44:26 +0700

    Create CustomExceptionHandler.java

---
+++ DemoSpringBoot/src/main/java/com/example/DemoSpringBoot/Exceptions/CustomExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.example.DemoSpringBoot.Exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler{
+    
+}
