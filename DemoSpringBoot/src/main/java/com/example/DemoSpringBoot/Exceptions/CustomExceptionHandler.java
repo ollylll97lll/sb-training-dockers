ec4232b0b20901a40a88e8c26f335194e0a0e1b9
ollylll97lll <58421531+ollylll97lll@users.noreply.github.com>
2022-07-15 10:49:47 +0700

    Finish first user story

--- DemoSpringBoot/src/main/java/com/example/DemoSpringBoot/Exceptions/CustomExceptionHandler.java
+++ DemoSpringBoot/src/main/java/com/example/DemoSpringBoot/Exceptions/CustomExceptionHandler.java
@@ -1,9 +1,14 @@
 package com.example.DemoSpringBoot.Exceptions;
 
+// import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+// import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler{
-    
+    // @ExceptionHandler(Exception.class)
+    // public final ResponseEntity<Response> handleAllExc(Exception ex){
+
+    // }
 }
